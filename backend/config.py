e493c9d6b46c64b10011c6136c7d6fd5c681158f
Amin <yazdi.mohammad99@gmail.com>
2023-10-05 17:47:25 +0330

    Add port to mongodb

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,7 @@ config_map = {
     'debug_mode': True,
     'server_port': 5000,
     'mongodb_host': 'mongodb://localhost/telewise',
+    'mongodb_port': 27018,
     'mongodb_name': 'telewise',
     'api_audience': 'http://localhost:5000/api/v1/terms',
     'algorithms': ["RS256"],
