ffa7e2c50b5c1defc296a1b8b2358715a37998a5
Amin <yazdi.mohammad99@gmail.com>
2023-10-06 00:04:36 +0330

    Fix models to User & Chat model.

--- backend/database/models.py
+++ backend/database/models.py
@@ -2,43 +2,49 @@ from .db import db
 import datetime
 
 
-# class Chat(db.Document):
-#     chat_id = db.StringField(required=True, unique=True)
+class Chat(db.Document):
+    chat_id = db.StringField(required=True, unique=True)
 
 
-# class User(db.Document):
-#     name = db.StringField(required=True)
-#     user_id = db.StringField(required=True, unique=True)
+class User(db.Document):
+    name = db.StringField(required=True)
+    user_id = db.StringField(required=True, unique=True)
 
 
 class Transaction(db.Document):
     transaction_id = db.SequenceField(required=True)
     title = db.StringField(required=True)
     description = db.StringField(max_length=64)
-    # chat = db.ReferenceField(Chat, required=True)
-    chat_id = db.StringField(required=True)
-    # payer_user = db.ReferenceField(User, required=True)
-    payer_user_id = db.StringField(required=True)
-    # in_debt_user = db.ReferenceField(User, required=True)
-    in_debt_user_id = db.StringField(required=True)
+    chat = db.ReferenceField(Chat, required=True)
+    # chat_id = db.StringField(required=True)
+    payer_user = db.ReferenceField(User, required=True)
+    # payer_user_id = db.StringField(required=True)
+    in_debt_user = db.ReferenceField(User, required=True)
+    # in_debt_user_id = db.StringField(required=True)
     amount = db.IntField(required=True)
     date = db.DateTimeField(required=True, default=datetime.datetime.utcnow)
 
 
 class Payment(db.Document):
-    # chat = db.ReferenceField(Chat, required=True)
-    chat_id = db.StringField(required=True)
-    # payer_user = db.ReferenceField(User, required=True)
-    payer_user_id = db.StringField(required=True)
-    # receiver_user = db.ReferenceField(User, required=True)
-    receiver_user_id = db.StringField(required=True)
+    chat = db.ReferenceField(Chat, required=True)
+    # chat_id = db.StringField(required=True)
+    payer_user = db.ReferenceField(User, required=True)
+    # payer_user_id = db.StringField(required=True)
+    receiver_user = db.ReferenceField(User, required=True)
+    # receiver_user_id = db.StringField(required=True)
     amount = db.IntField(required=True)
     date = db.DateTimeField(required=True, default=datetime.datetime.utcnow)
 
 
 class Peer(db.Document):
-    # chat = db.ReferenceField(Chat, required=True)
-    chat_id = db.StringField(required=True)
-    user_id = db.StringField(required=True)
-    # user = db.ReferenceField(User, required=True)
+    chat = db.ReferenceField(Chat, required=True)
+    # chat_id = db.StringField(required=True)
+    user = db.ReferenceField(User, required=True)
+    # user_id = db.StringField(required=True)
     date = db.DateTimeField(required=True, default=datetime.datetime.utcnow)
+
+class R(db.Document):
+    name = db.StringField(required=True)
+
+class B(db.Document):
+    rfe = db.ReferenceField(R, required=True)
--- backend/resources/user.py
+++ backend/resources/user.py
@@ -1,32 +1,49 @@
 from flask import Response, request
-# from database.models import Peer, User, Chat
-from database.models import Peer
+from database.models import Peer, User, Chat
+# from database.models import Peer
 from flask_restful import Resource
 # from flask_jwt_extended import jwt_required, get_jwt_identity
 # from utils.user import get_user_id
 from mongoengine.queryset.visitor import Q
 
 class UserApi(Resource):
-
     def post(self):
         body = request.get_json()
         if 'chat' in body and 'name' in body and 'userId' in body:
-            if 'userId already exists in chat':
-                return {'error': 'user already exists'}, 409
+            requested_user = User.objects.filter(user_id = body['userId'])
+            if len(requested_user) == 0:
+                requested_user = User(name = body['name'], user_id = body['userId'])
+                requested_user.save()
             else:
-                peer = Peer()
-                peer.chat_id = body['chat']
-                peer.user_id = body['userId']
+                requested_user = requested_user[0]
+            requested_chat = Chat.objects.filter(chat_id = body['chat'])
+            if len(requested_chat) == 0:
+                requested_chat = Chat(chat_id = body['chat'])
+                requested_chat.save()
+            else:
+                requested_chat = requested_chat[0]
+            requested_peer = Peer.objects.filter(chat=requested_chat, user=requested_user)
+            if len(requested_peer) == 0:
+                peer = Peer(chat=requested_chat, user=requested_user)
                 peer.save()
                 return {'msg': 'done'}, 200
+            else:
+                return {'Error': 'user already exists'}, 409
         else:
             return {"Error": "Missing Arguments (chat or name or userId)"}, 400
 
     
     def get(self):
         body = request.get_json()
-        users_in_peer = Peer.objects(Q(chat_id=body['chat']))
-        li = []
-        for user in users_in_peer:
-            li.append(user.user_id)
-        return {"{}".format(body['chat']): li}, 200
+        if 'chat' in body:
+            requested_chat = Chat.objects.filter(chat_id = body['chat'])
+            if len(requested_chat) > 0:
+                users_in_peer = Peer.objects.filter(chat=requested_chat[0])
+                li = []
+                for user_in_peer in users_in_peer:
+                    li.append({"userId": user_in_peer.user.user_id, "name": user_in_peer.user.name})
+                return {"{}".format(body['chat']): li}, 200
+            else:
+                return {"Error": "Chat does not exit."}, 404
+        else:
+            return {"Error": "Missing Argument (chat)"}, 400
