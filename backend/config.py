d15dcb7e7e29a4577c1a699ce6a2f4c80144aeb0
Amin <yazdi.mohammad99@gmail.com>
2023-10-06 01:42:31 +0330

    Remove unnecessary configs from backend config file.

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,4 @@ config_map = {
     'api_audience': 'http://localhost:5000/api/v1/terms',
     'algorithms': ["RS256"],
     'jwt_secret_key': 'super_secret_key_for_jwt',
-    'password_salt': 't3l3VViZ3_SaLt',
-    'tags_location' : "./static/tags/",
-    'github_accesstoken': "ghp_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
 }
