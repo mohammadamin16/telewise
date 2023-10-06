5b4050d70ba9746549e72ad4191bcc190b33906e
Amin Tootoonchi <toutounchi.ma@gmail.com>
2023-10-06 18:47:16 +0330

    Fixes router crash

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom", "axios"],
-    },
-  },
-  optimizeDeps: {
-    exclude: ["react-router-dom", "axios"],
-  },
-});
+})
