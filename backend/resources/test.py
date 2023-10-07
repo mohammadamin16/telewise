29d5c9c05314f8b2207d033c561a07e399ff7cc6
Amin <yazdi.mohammad99@gmail.com>
2023-10-08 01:03:55 +0330

    Root Clears DB.

--- backend/resources/test.py
+++ backend/resources/test.py
@@ -1,12 +1,12 @@
 from flask import Response, request
 from flask_restful import Resource
 from mongoengine.queryset.visitor import Q
-from database.models import R, B, Chat, Transaction, Payment, Peer, User
+from database.models import R, B, Chat, Transaction, Payment, Peer, User, Balance
 
 class testApi(Resource):
     def get(self):
-        r = R(name = "Hossein")
-        r.save()
+        # r = R(name = "Hossein")
+        # r.save()
         # b = B(rfe=r)
         # b.save()
         # r = R.objects.filter(name="Ali")
@@ -18,7 +18,18 @@ class testApi(Resource):
         #     print(type(item.rfe.name))
         # R.objects().delete()
         # Bransaction.objects().delete()
-
+        Chat.objects().delete()
+        User.objects().delete()
+        Transaction.objects().delete()
+        Payment.objects().delete()
+        Balance.objects().delete()
+        Peer.objects().delete()
+        R.objects().delete()
+        B.objects().delete()
+        print("Cleared")
         # return {'msg': "B:{}, R:{}".format(len(B.objects()), len(R.objects()))}, 200
-        return {'msg': "B:{}, R:{}".format(R.objects.first().name, len(Transaction.objects()))}, 200
+        return {'msg': "B:, R:"}, 200
         # return {'msg': "Done"}, 200
+
+
+
